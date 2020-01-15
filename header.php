25c39f61c7163168239491559bed1aadd85bbfcc
narayan bhardwaj <narayan3721@gmail.com>
2020-01-15 16:25:45 +0530

    menu

--- header.php
+++ header.php
@@ -20,11 +20,35 @@
             <a class="header__logo" href="#">
                 <svg width="55" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 60.03 43.15"><title>logo</title><text transform="translate(-0.16 34.46)" font-size="36" font-family="Lato-Medium, Lato" font-weight="500">N B</text><line x1="31.85" y1="1.9" x2="31.85" y2="39.9" stroke="#ff0" stroke-miterlimit="10" stroke-width="3"/></svg>
             </a>
-            <label for="header__navmenu" class="header__navmenu">
+            <input type="radio" id="navDrawer">
+            <label for="navDrawer" class="header__navmenu">
                 <span class="icon nb-bars-solid"></span> Menu
             </label>
-            <nav class="navbar">
-            </nav>
+            <div class="layout-drawer">
+                <div class="layout-drawer__userprofile">
+                    <div class="img">
+                        <img src="images/narayan-bhardwaj.jpeg">
+                    </div>
+                    <div class="userinfo">
+                        <a href="mailto:ui.narayanbhardwaj@gmail.com">ui.narayanbhardwaj@gmail.com</a>
+                    </div>
+                </div>
+                <nav class="navbar">
+                    <a href="#">
+                        <span class="icon nb-user-circle-regular"></span>
+                        <span>About Me</span>
+                    </a>
+                    <a href="#">
+                        <span class="icon nb-code-solid"></span>
+                        <span>All Projects</span>
+                    </a>
+                    <a href="#">
+                        <span class="icon nb-phone-alt-solid"></span>
+                        <span>Contact</span>
+                    </a>
+
+                </nav>
+            </div>
         </div>
     </div>
 </header>
