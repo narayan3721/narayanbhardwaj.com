69c9fc6b5c9d2c8b648323d114ce7a492e40ab7c
narayan bhardwaj <narayan3721@gmail.com>
2020-02-04 21:14:56 +0530

    new changes

--- index.php
+++ index.php
@@ -7,25 +7,26 @@ $characters = json_decode($data);
 
 <?php include("header.php"); ?>
 <main class="content">
+    <!--myinfo ends-->
     <section class="myinfo">
         <div class="container">
-            <div class="myinfo__content card">
+            <div class="myinfo__content">
                 <div class="myinfo__img">
                     <img src="images/narayan-bhardwaj.jpeg" alt="narayan-bhardwaj" width="255" height="255">
                 </div>
                 <div class="myinfo__info">
                     <h2>Hi, I am <span class="gradient-text">Narayan Bhardwaj</span>. <br>I am a <span class="gradient-text">UI Developer</span>.</h2>
                     <p>Have something to say? <span class="link-with-arrow"><a class="pr-4" href="/contacts">Let’s talk<span class="arrow"></span></a></span></p>
-</div>
+                </div>
             </div>
         </div>
     </section>
     <!--myinfo ends-->
 
+    <!--home projects-->
     <section class="home-projects">
-        
         <div class="container">
-        <h3 class="text-center">Projects </h3>
+            <h3 class="text-center">Projects </h3>
             <div class="siema">
                 <?php foreach ($characters as $character) : ?>
                     <div class="home-projects__card card">
@@ -43,8 +44,16 @@ $characters = json_decode($data);
                     </div>
                 <?php endforeach; ?>
             </div>
+            <!-- controls start-->
+            <div class="slider-controls">
+                <div class="prev"><span class="icon nb-chevron-circle-left-solid"></span></div>
+                <div class="next"><span class="icon nb-chevron-circle-right-solid"></span></div>
+            </div>    
+            <!-- controls end-->
         </div>
     </section>
+    <!--home projects-->
+
 </main>
 
 
@@ -53,25 +62,9 @@ $characters = json_decode($data);
 
 <script src="https://pawelgrzybek.com/siema/assets/siema.min.js"></script>
 <script>
-// Siema 1.3.0 introuduced a basic callbacks
-// https://github.com/pawelgrzybek/siema/releases/tag/v.1.3.0
-
-function onInitCallback() {
-  console.log('Siema initialised bro :)');
-}
-
-function onChangeCallback() {
-  console.log(`The index of current slide is: ${this.currentSlide}`);
-}
-
-const mySiema = new Siema({
-  onInit: onInitCallback,
-  onChange: onChangeCallback,
-});
-
-const prev = document.querySelector('.prev');
-const next = document.querySelector('.next');
-
-prev.addEventListener('click', () => mySiema.prev());
-next.addEventListener('click', () => mySiema.next());
+    const mySiema = new Siema({
+    selector: '.siema'
+    });
+    document.querySelector('.prev').addEventListener('click', () => mySiema.prev());
+    document.querySelector('.next').addEventListener('click', () => mySiema.next());
 </script>
